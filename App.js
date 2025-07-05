551ed034d387cc5355dda27d1443f1640c28e9d7
Arka <arka.das.se26@gmail.com>
2025-07-05 13:00:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// // Screen
+// import Login from './screens/Login';
+// import Signup from './screens/Signup';
+// import Welcome from './screens/Welcome';
+
+//React Navigator stack
+import RootStack from './Navigator/RootStack';
+
+export default function App() {
+  return <RootStack/>;
+}
+
+
